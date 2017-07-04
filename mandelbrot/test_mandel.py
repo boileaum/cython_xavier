c80a200b819f7ba29028064b223734dde2575a56
gouarin <loic.gouarin@gmail.com>
2017-07-04 09:54:03 +0200

    initial import

---
+++ mandelbrot/test_mandel.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import time
+
+import mandelbrot
+# import pyximport
+# pyximport.install()
+# import cython_mandelbrot as mandelbrot
+
+t1 = time.clock()
+mdl = mandelbrot.comp_mandelbrot( 1024, 768, 4 )
+t2 = time.clock()
+print("Temps calcul mandelbrot : {} secondes".format(t2-t1))
+imgplot = plt.imshow(mdl)
+plt.show()
